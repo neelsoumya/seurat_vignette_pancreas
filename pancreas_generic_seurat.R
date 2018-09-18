cdb5ed62dc540ad9ac7aecccb11204e5be0717ce
soumya banerjee <neel.soumya@gmail.com>
2018-09-18 16:12:04 +0000

    pancreas_generic_seurat.R edited online with Bitbucket

--- pancreas_generic_seurat.R
+++ pancreas_generic_seurat.R
@@ -55,23 +55,41 @@ pancreas.data <- new.data
 pancreas.md <- all.data[, 2:3]
 rownames(pancreas.md) <- all.data[, 1]
 
-###########################
-# 1) Filter, normalize
-# 2) Find variable genes
-# 3) Scale data
-# 4) PCA, tSNE, clusters
-###########################
+######################################################
+# Quality control and preprocessing
+# 1) Filter cells to get a number of cells after QC
+#       a. cells expressing fewer than 500 genes per cell
+#       b. genes expressed in less than 3 cells were removed from the analysis
+#
+# 2) Normalize
+# 3) Find variable genes
+# 4) Remove unwanted variation (use a linear model to remove unwanted variation)
+# 5) Run PCA on variable genes and tSNE also 
+# 6) Cluster the data on these principal components
+# 7) Find markers of these clusters
+######################################################
 
 pancreas.data <- Matrix(pancreas.data, sparse = T)
 pancreas <- CreateSeuratObject(raw.data = pancreas.data, min.cells = 3)
 pancreas <- AddMetaData(pancreas, metadata = pancreas.md)
+
+# Filter cells to get a number of cells after QC
+#       a. cells expressing fewer than 500 genes per cell
+#       b. genes expressed in less than 3 cells were removed from the analysis
 pancreas <- FilterCells(pancreas, subset.names = "nGene", low.thresholds = 500,  high.thresholds = Inf)
 pancreas <- NormalizeData(pancreas)
+
+# Find variable genes
 pancreas <- FindVariableGenes(pancreas, x.low.cutoff = 0.1)
+
+# Remove unwanted variation (use a linear model to remove unwanted variation)
 pancreas <- ScaleData(pancreas, vars.to.regress = c("orig.ident", "nUMI"), genes.use = pancreas@var.genes, model.use = "negbinom")
 
+# Run PCA on variable genes and tSNE also 
 pancreas <- RunPCA(pancreas, pcs.compute = 30, weight.by.var = FALSE)
 pancreas <- RunTSNE(pancreas, dims.use = 1:19, do.fast = T)
+
+# Cluster the data on these principal components
 pancreas <- FindClusters(pancreas, reduction.type = "pca", dims.use = 1:19, save.SNN = T)
 
 # color by cluster ID, annotated cluster from the manuscript, or batch
@@ -92,6 +110,7 @@ TSNEPlot(pancreas, group.by = "orig.ident")
 
 # Find Markers of ductal cell subcluster, using the negative binomial test
 # only test genes with a 20% difference in detection rate to speed-up (optional)
+# Find markers of these clusters
 ductal.markers <- FindMarkers(pancreas, ident.1 = 5, ident.2 = 12, test.use = "negbinom", min.diff.pct = 0.2)
 
 
